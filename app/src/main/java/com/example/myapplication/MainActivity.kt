fd03cb26c88e4db2f84c952d142f5e14cef6f785
Biratal Wagle <46479625+xerader@users.noreply.github.com>
2024-10-29 21:18:50 -0400

    adds top 10 package detection. Reverts package name detection

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -66,49 +66,65 @@ fun Context.requestUsageStatsPermission() {
     startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
 }
 
+// get top 10 most used apps in the last month
+fun topUsedApps(context: Context): List<UsageStats> {
+    val usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+    val calStart = Calendar.getInstance().apply {
+        add(Calendar.MONTH, -1) // Set to one month ago
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+    }
+    val calEnd = Calendar.getInstance() // Set to current time
+
+    val usageStats = usageStatsManager.queryUsageStats(
+        UsageStatsManager.INTERVAL_YEARLY,
+        calStart.timeInMillis,
+        calEnd.timeInMillis
+    )
+
+    // Sort the apps by total usage time in descending order and get the top 10
+    return usageStats
+//        .filter { !it.packageName.contains("android", ignoreCase = true) && !it.packageName.contains("frontpage", ignoreCase = true) }
+        .sortedByDescending { it.totalTimeInForeground }
+        .take(10)
+
+}
+
+
+
 
 fun getUsageStats(context: Context): List<UsageStats> {
     val usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
-    val cal = Calendar.getInstance()
-    cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR) - 10)
-    cal.set(Calendar.HOUR_OF_DAY, 0)
-    cal.set(Calendar.MINUTE, 0)
-    cal.set(Calendar.SECOND, 0)
-    cal.set(Calendar.MILLISECOND, 0)
-    return usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST, cal.timeInMillis, System.currentTimeMillis())
-}
-
-fun getUsageEvents(context: Context): UsageEvents {
-    val usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
-    val cal = Calendar.getInstance()
-    cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR) - 14)
-    cal.set(Calendar.HOUR_OF_DAY, 0)
-    cal.set(Calendar.MINUTE, 0)
-    cal.set(Calendar.SECOND, 0)
-    cal.set(Calendar.MILLISECOND, 0)
-    println("${cal.timeInMillis}, ${System.currentTimeMillis()}")
-    return usageStatsManager.queryEvents(cal.timeInMillis, System.currentTimeMillis())
-}
-
-fun printUsageEvents(context: Context) {
-    val usageEvents = getUsageEvents(context)
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-    val statsBuilder = StringBuilder()
-    while (usageEvents.hasNextEvent()) {
-        val event = UsageEvents.Event()
-        usageEvents.getNextEvent(event)
-        val date = dateFormat.format(event.timeStamp)
-        statsBuilder.append("Package: ${event.packageName}, Type: ${event.eventType}, Time: $date\n")
+    val calStart = Calendar.getInstance().apply {
+        add(Calendar.DAY_OF_YEAR, -1) // Set to the previous day
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
     }
-    println(statsBuilder.toString())
+    val calEnd = Calendar.getInstance().apply {
+        add(Calendar.DAY_OF_YEAR, -1) // Same day as start
+        set(Calendar.HOUR_OF_DAY, 23)
+        set(Calendar.MINUTE, 59)
+        set(Calendar.SECOND, 59)
+        set(Calendar.MILLISECOND, 999)
+    }
+    return usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, calStart.timeInMillis, calEnd.timeInMillis)
 }
 
 
-
-fun runContext(context: Context): ArrayList<String> {
+fun runContext(context: Context, top: Boolean): ArrayList<String> {
     val resultList = ArrayList<String>()
     if (context.hasUsageStatsPermission()) {
-        val usageStatsList = getUsageStats(context)
+        val usageStatsList: List<UsageStats>
+        if (top){
+            usageStatsList = topUsedApps(context)
+        }
+        else {
+            usageStatsList = getUsageStats(context)
+        }
         Toast.makeText(context, "Usage Stats permission is obtained!", Toast.LENGTH_LONG).show()
 
         val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
@@ -147,10 +163,10 @@ fun runContext(context: Context): ArrayList<String> {
                 usageStats.packageName.substringAfterLast('.').split("(?<=.)(?=\\p{Upper})".toRegex())
                     .joinToString(" ").capitalize()
             }
-            val date = dateFormat.format(usageStats.lastTimeUsed)
+            val date = dateFormat.format(usageStats.firstTimeStamp)
 
             // get the usageStats output in a readable format
-            val packageName = appName
+            val packageName = usageStats.packageName
             val totalTimeInMins = usageStats.totalTimeInForeground / (1000 * 60)
 
 
@@ -172,8 +188,7 @@ fun runContext(context: Context): ArrayList<String> {
 
 
 class MainActivity : ComponentActivity() {
-
-    val todayDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+//get the calendar date
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -185,8 +200,10 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Bob")
-                    val resList = runContext(this)
-                    writeTextFileToDocuments(this, todayDate, resList.joinToString("\n"))
+                    val resList = runContext(this, true)
+                    // whatever the date is in the first entry in reslist
+                    var title = resList[0].split(",")[2]
+                    writeTextFileToDocuments(this, title, resList.joinToString("\n"))
                     }
                                     }
             }
